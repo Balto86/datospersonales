a75be628a4a2c2c1d5e3a423d797ed8f63a0a4db
david cargua <df.cargual@uea.edu.ec>
2024-02-27 16:17:58 -0500

    deber 13

--- promedio temperatura.py
+++ promedio temperatura.py
@@ -1,6 +1,8 @@
-temperaturas = [
-    [   # Ciudad Quito
-        [   # Semana 1
+from iteraciones import promedio, ciudad
+
+ciudades_temperaturas = [
+    [  # quito
+        [  # Semana 1
             {"day": "Lunes", "temp": 35},
             {"day": "Martes", "temp": 23},
             {"day": "Miércoles", "temp": 22},
@@ -9,7 +11,7 @@ temperaturas = [
             {"day": "Sábado", "temp": 28},
             {"day": "Domingo", "temp": 32}
         ],
-        [   # Semana 2
+        [  # Semana 2
             {"day": "Lunes", "temp": 16},
             {"day": "Martes", "temp": 29},
             {"day": "Miércoles", "temp": 33},
@@ -18,7 +20,7 @@ temperaturas = [
             {"day": "Sábado", "temp": 29},
             {"day": "Domingo", "temp": 33}
         ],
-        [   # Semana 3
+        [  # Semana 3
             {"day": "Lunes", "temp": 27},
             {"day": "Martes", "temp": 31},
             {"day": "Miércoles", "temp": 35},
@@ -27,7 +29,7 @@ temperaturas = [
             {"day": "Sábado", "temp": 31},
             {"day": "Domingo", "temp": 45}
         ],
-        [   # Semana 4
+        [  # Semana 4
             {"day": "Lunes", "temp": 25},
             {"day": "Martes", "temp": 38},
             {"day": "Miércoles", "temp": 10},
@@ -37,8 +39,8 @@ temperaturas = [
             {"day": "Domingo", "temp": 21}
         ]
     ],
-    [   # Puyo 2
-        [   # Semana 1
+    [  # puyo
+        [  # Semana 1
             {"day": "Lunes", "temp": 32},
             {"day": "Martes", "temp": 44},
             {"day": "Miércoles", "temp": 28},
@@ -47,7 +49,7 @@ temperaturas = [
             {"day": "Sábado", "temp": 45},
             {"day": "Domingo", "temp": 39}
         ],
-        [   # Semana 2
+        [  # Semana 2
             {"day": "Lunes", "temp": 43},
             {"day": "Martes", "temp": 36},
             {"day": "Miércoles", "temp": 40},
@@ -56,7 +58,7 @@ temperaturas = [
             {"day": "Sábado", "temp": 37},
             {"day": "Domingo", "temp": 31}
         ],
-        [   # Semana 3
+        [  # Semana 3
             {"day": "Lunes", "temp": 31},
             {"day": "Martes", "temp": 45},
             {"day": "Miércoles", "temp": 58},
@@ -65,7 +67,7 @@ temperaturas = [
             {"day": "Sábado", "temp": 36},
             {"day": "Domingo", "temp": 40}
         ],
-        [   # Semana 4
+        [  # Semana 4
             {"day": "Lunes", "temp": 44},
             {"day": "Martes", "temp": 37},
             {"day": "Miércoles", "temp": 49},
@@ -75,8 +77,8 @@ temperaturas = [
             {"day": "Domingo", "temp": 40}
         ]
     ],
-    [   # Cuenca 3
-        [   # Semana 1
+    [  # cuenca
+        [  # Semana 1
             {"day": "Lunes", "temp": 30},
             {"day": "Martes", "temp": 22},
             {"day": "Miércoles", "temp": 24},
@@ -85,7 +87,7 @@ temperaturas = [
             {"day": "Sábado", "temp": 18},
             {"day": "Domingo", "temp": 22}
         ],
-        [   # Semana 2
+        [  # Semana 2
             {"day": "Lunes", "temp": 29},
             {"day": "Martes", "temp": 21},
             {"day": "Miércoles", "temp": 13},
@@ -94,7 +96,7 @@ temperaturas = [
             {"day": "Sábado", "temp": 24},
             {"day": "Domingo", "temp": 21}
         ],
-        [   # Semana 3
+        [  # Semana 3
             {"day": "Lunes", "temp": 21},
             {"day": "Martes", "temp": 23},
             {"day": "Miércoles", "temp": 35},
@@ -103,7 +105,7 @@ temperaturas = [
             {"day": "Sábado", "temp": 36},
             {"day": "Domingo", "temp": 23}
         ],
-        [   # Semana 4
+        [  # Semana 4
             {"day": "Lunes", "temp": 28},
             {"day": "Martes", "temp": 10},
             {"day": "Miércoles", "temp": 22},
@@ -115,9 +117,17 @@ temperaturas = [
     ]
 ]
 
-ciudades = ["Quito 1", "Puyo 2", "Cuenca 3"]
-for ciudad_idx, ciudad in enumerate(temperaturas):
-    for semana_idx, semana in enumerate(ciudad):
-        suma_temperaturas = sum([dia["temp"] for dia in semana])
-        promedio = suma_temperaturas / len(semana)
-        print(f"Promedio de temperaturas en {ciudades[ciudad_idx]}, Semana {semana_idx + 1}: {promedio:.2f} grados")
+
+def temperatura_promedio(promedios_temperaturas):
+    temperaturas_promedio = []
+
+    for ciudad, temperaturas in promedios_temperaturas.items():
+        promedio = sum(temperaturas) / len(temperaturas)
+        temperaturas_promedio[ciudad] = promedio
+
+    return temperaturas_promedio
+
+
+print("Temperaturas por Ciudad:")
+for ciudades, promedios in "ciudades_temperaturas":
+    print(f"{ciudad}: {promedio:.2f}°C")
