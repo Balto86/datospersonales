ca9d9ebdf68534c208a27292b0bce5c0b3b1a811
david cargua <df.cargual@uea.edu.ec>
2024-03-21 19:31:31 -0500

    deber 15

---
+++ diccionario.py
@@ -0,0 +1,9 @@
+informacion_personal = {"nombres": "David", "edad": "38", "ciudad": "Quito", "profesion": "Tecnologo"}
+print(informacion_personal)
+informacion_personal["ciudad"] = "Puyo"
+informacion_personal["titulo"] = "Tecnologo"
+print(informacion_personal)
+informacion_personal["telefono"] = "0987398025"
+print(informacion_personal)
+del (informacion_personal["edad"])
+print(informacion_personal)
