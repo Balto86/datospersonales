1dfd606d64232d68e17d1ad8f51c8d2af650331b
david cargua <df.cargual@uea.edu.ec>
2024-03-30 00:07:24 -0500

    deber 15

---
+++ archivos de texto.py
@@ -0,0 +1,6 @@
+with open("my_notes.txt", "w") as file_object:
+    file_object.write("mi nombre es david cargua \n")
+    file_object.write("tengo 38  años \n")
+    file_object.write("estudiante de tecnologias de la informacion\n")
+file_object.close()
+
